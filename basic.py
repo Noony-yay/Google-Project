ca9c5ff26f878fdb2dcc3959394d535d01b17e2e
NYS <noyayabrovsalomon@gmail.com>
2025-03-31 20:20:51 +0300

    Update basic.py
    no bug and calc_score is finished (yay)
    
    Naama - you need to create the cards (if you want  I can do it)
    Karin and Avital - I'm looking forward to see your function (-:

--- basic.py
+++ basic.py
@@ -1,9 +1,11 @@
+from typing import List # I only understood that the problem was that the phyton here isn't in the newest version so we needed to import "List"
+
 class Card:
   def __init__(self, num, t):
     self.number: int = num
     self.t: str = t
 
-def calc_score(cards: list[Card]) -> int:
+def calc_score(cards: List[Card]) -> int: # here is the only change
   types = {
     "clubs": 0,
     "spades": 0,
@@ -12,7 +14,12 @@ def calc_score(cards: list[Card]) -> int:
   }
   for card in cards:
     types[card.t] += 1
-   
-  return types
-  
+
+  #I also added the calculation of the score :-)
+  score = 0
+  for count in types.values():
+    if count > 0:
+      score += 2 ** count
+  return score
+
 print(calc_score([Card(3, "hearts"), Card(4, "hearts"), Card(2, "spades")]))
